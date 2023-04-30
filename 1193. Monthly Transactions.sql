5dcae57d638035ff3af600b6d97e8a17970f97d6
MordecaiO <cai.oladimeji@gmail.com>
2023-04-30 22:34:12 +0100

    Update 1193. Monthly Transactions.sql

--- 1193. Monthly Transactions.sql
+++ 1193. Monthly Transactions.sql
@@ -41,3 +41,24 @@ Output:
 | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
 +----------+---------+-------------+----------------+--------------------+-----------------------+
 */
+SELECT 
+DATE_FORMAT(trans_date, "%Y-%m") AS month, 
+country, 
+COUNT(id) AS trans_count,
+COUNT(
+    CASE
+    WHEN state='approved' THEN 1
+    ELSE NULL
+    END
+) AS approved_count, 
+SUM(amount) AS trans_total_amount,
+SUM(
+    CASE
+    WHEN state='approved' THEN amount
+    ELSE 0
+    END
+) AS approved_total_amount
+FROM transactions
+GROUP BY country, month 
+
+
