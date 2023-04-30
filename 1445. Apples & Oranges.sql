dd7e4d142f0f3aeffd07e30c21d180c40bce9ad8
MordecaiO <cai.oladimeji@gmail.com>
2023-04-30 19:07:52 +0100

    Update 1445. Apples & Oranges.sql

--- 1445. Apples & Oranges.sql
+++ 1445. Apples & Oranges.sql
@@ -1,1 +1,46 @@
+/*
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| sale_date     | date    |
+| fruit         | enum    | 
+| sold_num      | int     | 
++---------------+---------+
+(sale_date, fruit) is the primary key for this table.
+This table contains the sales of "apples" and "oranges" sold each day.
+ 
 
+Write an SQL query to report the difference between the number of apples and oranges sold each day.
+
+Return the result table ordered by sale_date.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++------------+------------+-------------+
+| sale_date  | fruit      | sold_num    |
++------------+------------+-------------+
+| 2020-05-01 | apples     | 10          |
+| 2020-05-01 | oranges    | 8           |
+| 2020-05-02 | apples     | 15          |
+| 2020-05-02 | oranges    | 15          |
+| 2020-05-03 | apples     | 20          |
+| 2020-05-03 | oranges    | 0           |
+| 2020-05-04 | apples     | 15          |
+| 2020-05-04 | oranges    | 16          |
++------------+------------+-------------+
+Output: 
++------------+--------------+
+| sale_date  | diff         |
++------------+--------------+
+| 2020-05-01 | 2            |
+| 2020-05-02 | 0            |
+| 2020-05-03 | 20           |
+| 2020-05-04 | -1           |
++------------+--------------+
+*/
