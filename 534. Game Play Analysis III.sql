bbb1ac6be5484491d72c30aa4dd81a01fede6b07
MordecaiO <cai.oladimeji@gmail.com>
2023-05-15 08:01:06 +0100

    Update 534. Game Play Analysis III.sql

--- 534. Game Play Analysis III.sql
+++ 534. Game Play Analysis III.sql
@@ -14,7 +14,8 @@ This table shows the activity of players of some games.
 Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
  
 
-Write an SQL query to report for each player and date, how many games played so far by the player. That is, the total number of games played by the player until that date. Check the example for clarity.
+Write an SQL query to report for each player and date, how many games played so far by the player. 
+That is, the total number of games played by the player until that date. Check the example for clarity.
 
 Return the result table in any order.
 
