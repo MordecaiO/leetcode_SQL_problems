d1f8afa4e7d740e9262062615d9131470cbec636
MordecaiO <cai.oladimeji@gmail.com>
2023-05-22 08:12:55 +0100

    Update 1097. Game Play Analysis V.sql

--- 1097. Game Play Analysis V.sql
+++ 1097. Game Play Analysis V.sql
@@ -43,3 +43,13 @@ Output:
 Explanation: 
 Player 1 and 3 installed the game on 2016-03-01 but only player 1 logged back in on 2016-03-02 so the day 1 retention of 2016-03-01 is 1 / 2 = 0.50
 Player 2 installed the game on 2017-06-25 but didn't log back in on 2017-06-26 so the day 1 retention of 2017-06-25 is 0 / 1 = 0.00*/
+
+SELECT install_dt, COUNT(a1.player_id) AS installs,
+ROUND(
+  COUNT(a2.event_date)/COUNT(a1.player_id),2
+) AS Day1_retention
+FROM (SELECT  player_id , MIN(event_date) AS install_dt FROM Activity  
+GROUP BY player_id) a1 
+LEFT JOIN Activity a2 ON a1.player_id = a2.player_id 
+AND a1.install_dt + 1 = a2.event_date
+GROUP BY install_dt
