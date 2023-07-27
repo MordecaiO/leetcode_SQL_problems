aa3cbc11728af8a4151508d96a0abe8af2d3355b
MordecaiO <cai.oladimeji@gmail.com>
2023-07-27 07:29:01 +0100

    Update 1661. Average Time of Process per Machine.sql

--- 1661. Average Time of Process per Machine.sql
+++ 1661. Average Time of Process per Machine.sql
@@ -27,3 +27,11 @@ The resulting table should have the machine_id along with the average time as pr
 Return the result table in any order.
 
 The query result format is in the following example.*/
+
+# Write your MySQL query statement below
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp),3) AS processing_time 
+FROM activity a1 
+JOIN activity a2 ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id 
+AND a1.activity_type = 'start' AND a2.activity_type = 'end'
+GROUP BY a1.machine_id
+
