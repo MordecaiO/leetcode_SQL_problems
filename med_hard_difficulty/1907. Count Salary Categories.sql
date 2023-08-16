f26226bb42d4507de942c410565fdf5528f8a337
MordecaiO <cai.oladimeji@gmail.com>
2023-08-16 05:18:51 +0100

    Update 1907. Count Salary Categories.sql

--- med_hard_difficulty/1907. Count Salary Categories.sql
+++ med_hard_difficulty/1907. Count Salary Categories.sql
@@ -19,4 +19,29 @@ The result table must contain all three categories. If there are no accounts in 
 
 Return the result table in any order.
 
+*/
+WITH salary_ranges as ( 
+    SELECT account_id, income, CASE
+    WHEN income < 20000 THEN "Low Salary"
+    WHEN income >= 20000 
+    AND income <= 50000 THEN "Average Salary"
+    WHEN income > 50000 THEN "High Salary"
+    END as category FROM Accounts
+), 
 
+salary_counts AS (
+SELECT category, COUNT(account_id) as accounts_count FROM salary_ranges GROUP BY category
+), 
+
+categories (category) AS (
+    (SELECT "Low Salary") 
+    UNION (SELECT "Average Salary")
+    UNION (SELECT "High Salary")
+) 
+
+SELECT c.category, 
+CASE WHEN sc.accounts_count IS NULL THEN 0
+ELSE sc.accounts_count END AS accounts_count
+FROM categories c 
+LEFT JOIN salary_counts sc ON 
+c.category = sc.category 
